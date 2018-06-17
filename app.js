60230a44060a8683f901cffe6e977b1cfad30de3
woodylan <woodylan@126.com>
2018-06-17 08:50:30 +0800

    全局url

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    //域名
+    this.globalData.domain = 'https://test-time.wugenglong.com/'
 
     // 登录
     wx.login({
@@ -21,26 +19,7 @@ App({
         })
       }
     })
-    // // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+  
   },
   globalData: {
     userInfo: null
@@ -48,7 +27,7 @@ App({
 
   postRequest: function (inputData){
     wx.request({
-      url: 'https://test-time.wugenglong.com/api/weapp/v1/user/login',
+      url: this.globalData.domain+'api/weapp/v1/user/login',
       data: { "data": JSON.stringify(inputData)},
       header: {
         'Content-Type': 'application/x-www-form-urlencoded',
