b57359c0568628463cde15d90eb671d39818cf38
woodylan <woodylan@126.com>
2018-06-17 17:32:50 +0800

    封装请求接口

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //app.js
+var CONFIG = require("./config");
 App({
-  onLaunch: function () {
-    //域名
-    this.globalData.domain = 'https://test-time.wugenglong.com/'
+  globalData: {
+    userInfo: null,
+    auth: ''
+  },
 
+  onLaunch: function () {
     // 登录
     wx.login({
       success: loginRes => {
@@ -12,34 +15,32 @@ App({
         let _code = loginRes.code
         wx.getUserInfo({
           success: function (userInfoRes) {
-            var data = { 'code': _code, 'iv': userInfoRes.iv, 'encryptedData': userInfoRes.encryptedData}
-            _this.postRequest(data)
-            console.log(userInfoRes)
+            var data = { 'code': _code, 'iv': userInfoRes.iv, 'encryptedData': userInfoRes.encryptedData }
+            _this.postRequest(CONFIG.ACTION.USER.LOGIN, data,function(res){
+              _this.globalData.auth = res.data.auth
+            })
           }
         })
       }
     })
-  
-  },
-  globalData: {
-    userInfo: null
   },
 
-  postRequest: function (inputData){
+  postRequest: function (action, inputData, callback) {
     wx.request({
-      url: this.globalData.domain+'api/weapp/v1/user/login',
-      data: { "data": JSON.stringify(inputData)},
+      url: CONFIG.API_URL + action,
+      data: { "data": JSON.stringify(inputData) },
       header: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       method: 'POST',
       dataType: 'json',
       responseType: 'text',
-      success: function(res) {
+      success: function (res) {
+        callback(res.data);
         console.log(res)
       },
-      fail: function(res) {},
-      complete: function(res) {},
+      fail: function (res) { },
+      complete: function (res) { },
     })
-  }
+  },
 })
---
+++ config.js
@@ -0,0 +1,14 @@
+exports.API_URL = "https://test-time.wugenglong.com/api/weapp/v1/";
+exports.ACTION = {
+  CHECK_AUTH: 'account/check-auth',
+  USER: {
+    LOGIN: 'user/login'
+  },
+  TIME: {
+    ADD: 'time/save',
+    DELETE: 'time/delete',
+    LIST: 'time/list',
+    SINGLE: 'time/detail',
+    LIKE: 'time/like'
+  }
+};
