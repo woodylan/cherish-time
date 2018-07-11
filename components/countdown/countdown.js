df523b23f54892d2ec8c1934ca5937f3adf117bc
woodylan <woodylan@126.com>
2018-07-11 19:44:33 +0800

    添加时间详情流components

---
+++ components/countdown/countdown.js
@@ -0,0 +1,27 @@
+// components/countdown/countdown.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    days: String,
+    timeType: Number,
+    timeName: String,
+    remark: String,
+    color: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
